f2e53b02c0b3732788422d64e206327cffba6e0d
Tristan B. Velloza Kildaire <deavmi@redxen.eu>
2025-05-16 16:45:52 +0200

    Package
    - Documented

--- source/qix/package.d
+++ source/qix/package.d
@@ -1,4 +1,18 @@
+/**
+ * Qix 
+ *
+ * Simple waitable-queue management
+ *
+ * Authors: Tristan Brice Velloza Kildaire (deavmi)
+ */
 module qix;
 
+/**
+ * Definitions for queues
+ */
 public import qix.queue;
+
+/**
+ * Queue management
+ */
 public import qix.manager;
